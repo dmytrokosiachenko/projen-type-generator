d2690e7d4d6141b2fb42ae7b68dd10be6082d2ab
Kosiachenko <dkosiac@cmtclm2dt07lmd6.rd.corpintra.net>
2023-05-16 09:16:35 +0200

    copy pasta fix

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,7 +7,7 @@ const devDeps = ['@types/js-yaml@^4.0.5', '@types/readable-stream'];
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   description: 'Generates typescript interfaces out of JSON or YAML files in projen.',
-  keywords: ['awscdk', 'cdk', 'AWS Step Functions'],
+  keywords: ['awscdk', 'cdk'],
   name: 'projen-type-generator',
   peerDeps: ['projen'],
   deps: deps,
